24c57bc7819df0b4dd7d0e905d00a02049bb0374
abhinavyadav <yadavabhinavkumar@gmail.com>
2023-04-29 11:54:45 +0530

    commit

--- server.js
+++ server.js
@@ -8,4 +8,6 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(9000);
+app.listen(9000,()=>{
+  console.log("Serving on port 9000")
+});
